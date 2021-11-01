bb4cf4770bd4b2b1c278ac144c23772fe4a307c9
Azeem Adenuga <adenugaazeem80@gmail.com>
2021-11-01 13:53:00 +0100

    update to order table

--- database/migrations/2020_10_02_095012_create_orders_table.php
+++ database/migrations/2020_10_02_095012_create_orders_table.php
@@ -18,25 +18,25 @@ class CreateOrdersTable extends Migration
             $table->id();
             $table->unsignedBigInteger('user_id');
             $table->foreign('user_id')
-            ->references('id')->on('users');
+                ->references('id')->on('users');
             $table->unsignedBigInteger('meal_id');
             $table->foreign('meal_id')
-            ->references('id')->on('meals');
+                ->references('id')->on('meals');
             $table->unsignedBigInteger('shop_id');
             $table->foreign('shop_id')
-            ->references('id')->on('shops');
+                ->references('id')->on('shops');
             $table->unsignedInteger('quantity')->default(1);
             $table->unsignedBigInteger('meal_size_id');
             $table->foreign('meal_size_id')
-            ->references('id')->on('meal_sizes');
-            $table->string('billingAddress');
-            $table->string('shippingAddress');
-            $table->enum('paymentType', ['bank-transfer', 'credit-card']);
+                ->references('id')->on('meal_sizes');
+            $table->string('billing_address');
+            $table->string('shipping_address');
+            $table->enum('payment_type', ['bank-transfer', 'credit-card']);
             $table->text('order_status', ['delivery not started', 'delivery in progress', 'delivered'])->default('delivery not started');
-            $table->string('cardType')->nullable();
-            $table->string('nameOnCard')->nullable();
-            $table->string('cardNumber')->nullable();
-            $table->string('cardExpiration')->nullable();
+            $table->string('card_type')->nullable();
+            $table->string('name_on_card')->nullable();
+            $table->string('card_number')->nullable();
+            $table->string('card_expiration')->nullable();
             $table->string('ccv')->nullable();
             $table->boolean('is_delivered')->default(false);
             $table->string('promo_code')->nullable();
